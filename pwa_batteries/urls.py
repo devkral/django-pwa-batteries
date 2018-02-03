dcccadb653096d47b0916fbb91b296f088849820
alex <devkral@web.de>
2018-02-04 00:01:16 +0100

    fix wrong name

--- pwa_batteries/urls.py
+++ pwa_batteries/urls.py
@@ -3,5 +3,5 @@ from .views import EndpointJson
 
 # Serve up serviceworker.js and manifest.json at the root
 urlpatterns = [
-    path('endpoint/json', FetchJson.as_view(), name="pwa_endpoint_json"),
+    path('endpoint/json', EndpointJson.as_view(), name="pwa_endpoint_json"),
 ]
